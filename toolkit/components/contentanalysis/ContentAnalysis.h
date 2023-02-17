91bbe9ce10da86e950e5084a8038b33907dfaf26
David Parks <daparks@mozilla.com>
2023-02-17 12:03:21 -0800

    WIP: Bug 1811076: Integrate content analysis client
    Differential Revision: https://phabricator.services.mozilla.com/D165752

---
+++ toolkit/components/contentanalysis/ContentAnalysis.h
@@ -0,0 +1,138 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#ifndef mozilla_contentanalysis_h
+#define mozilla_contentanalysis_h
+
+#include "mozilla/DataMutex.h"
+#include "nsIContentAnalysis.h"
+#include "nsString.h"
+
+#include <string>
+
+namespace content_analysis::sdk {
+class Client;
+class ContentAnalysisRequest;
+class ClientDownloadRequest_Resource;
+class ContentAnalysisResponse;
+class ContentAnalysisAcknowledgement;
+}  // namespace content_analysis::sdk
+
+namespace mozilla {
+namespace contentanalysis {
+
+class ContentAnalysis;
+
+class ClientDownloadResource : public nsIClientDownloadResource {
+ public:
+  NS_DECL_THREADSAFE_ISUPPORTS
+  NS_DECL_NSICLIENTDOWNLOADRESOURCE
+
+  ClientDownloadResource(nsAString&& aUrl, unsigned long aResourceType);
+
+ private:
+  virtual ~ClientDownloadResource() = default;
+
+  nsString mUrl;
+  unsigned long mResourceType;
+};
+
+class ContentAnalysisRequest : public nsIContentAnalysisRequest {
+ public:
+  NS_DECL_THREADSAFE_ISUPPORTS
+  NS_DECL_NSICONTENTANALYSISREQUEST
+
+  ContentAnalysisRequest(unsigned long aAnalysisType, nsAString&& aString,
+                         bool aStringIsFilePath);
+
+ private:
+  virtual ~ContentAnalysisRequest() = default;
+
+  // See nsIContentAnalysisRequest for values
+  unsigned long mAnalysisType;
+
+  // Text content to analyze.  Only one of textContent or filePath is defined.
+  nsString mTextContent;
+
+  // Name of file to analyze.  Only one of textContent or filePath is defined.
+  nsString mFilePath;
+
+  // The URL containing the file download/upload or to which web content is
+  // being uploaded.
+  nsString mUrl;
+
+  // Sha256 digest of file.
+  nsCString mSha256Digest;
+
+  // URLs involved in the download.
+  nsTArray<RefPtr<nsIClientDownloadResource>> mResources;
+
+  // Email address of user.
+  nsString mEmail;
+};
+
+class ContentAnalysisResponse : public nsIContentAnalysisResponse {
+ public:
+  NS_DECL_THREADSAFE_ISUPPORTS
+  NS_DECL_NSICONTENTANALYSISRESPONSE
+
+  static RefPtr<ContentAnalysisResponse> FromProtobuf(
+      content_analysis::sdk::ContentAnalysisResponse&& aResponse);
+
+  void SetOwner(RefPtr<ContentAnalysis> aOwner);
+
+ private:
+  virtual ~ContentAnalysisResponse() = default;
+  ContentAnalysisResponse(
+      content_analysis::sdk::ContentAnalysisResponse&& aResponse);
+
+  // See nsIContentAnalysisResponse for values
+  unsigned long mAction;
+
+  std::string mRequestToken;
+
+  // ContentAnalysis (or, more precisely, it's Client object) must outlive
+  // the transaction.
+  RefPtr<ContentAnalysis> mOwner;
+};
+
+class ContentAnalysisAcknowledgement
+    : public nsIContentAnalysisAcknowledgement {
+ public:
+  NS_DECL_THREADSAFE_ISUPPORTS
+  NS_DECL_NSICONTENTANALYSISACKNOWLEDGEMENT
+
+ private:
+  virtual ~ContentAnalysisAcknowledgement();
+
+  // See nsIContentAnalysisAcknowledgement for values
+  unsigned long mResult;
+  unsigned long mFinalAction;
+};
+
+class ContentAnalysis : public nsIContentAnalysis {
+ public:
+  NS_DECL_THREADSAFE_ISUPPORTS
+  NS_DECL_NSICONTENTANALYSIS
+
+  nsresult RunAcknowledgeTask(
+      nsIContentAnalysisAcknowledgement* aAcknowledgement,
+      const std::string& aRequestToken);
+
+  ContentAnalysis() = default;
+
+ private:
+  virtual ~ContentAnalysis();
+  nsresult EnsureContentAnalysisClient();
+  nsresult RunAnalyzeRequestTask(RefPtr<nsIContentAnalysisRequest> aRequest,
+                                 RefPtr<mozilla::dom::Promise> aPromise);
+
+  static StaticDataMutex<UniquePtr<content_analysis::sdk::Client>> sCaClient;
+};
+
+}  // namespace contentanalysis
+}  // namespace mozilla
+
+#endif  // mozilla_contentanalysis_h
